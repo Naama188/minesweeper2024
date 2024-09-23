1b9bc0333ccf0162c70d0c1d7cb92465b90f0a5a
Naama188 <51750001+Naama188@users.noreply.github.com>
2024-09-23 21:21:51 +0300

    second upload

--- js/main.js
+++ js/main.js
@@ -2,11 +2,12 @@
 
 const MINE = '💣'
 const FLAG = '🚩'
-const SMILEYNORMAL = '😄' 
-const SMILEYDEAD = '🤯' 
+const SMILEYNORMAL = '😄'
+const SMILEYDEAD = '🤯'
 const SMILEYWIN = '😎'
 const LIFE = '💗'
 const HINT = '💡'
+const USED_HINT = '🔅'
 
 var gBoard = []
 
@@ -20,7 +21,9 @@ var gLevels = [
 
 var gLevel = gLevels[0]
 
-var gLifeLost = 0
+var gLifeLost
+var gHintUsed
+var gHintIsOn
 
 var gGame = {
     isOn: false,
@@ -31,6 +34,12 @@ var gGame = {
 
 var gGameInterval
 var gIsvictory = false
+var gBestScoreLevel = 'bestScore' + gLevel.SIZE
+var display = document.querySelector('.time')
+var isRuning = false
+var startTime = 0
+var timer = null
+var elapsedTime = 0 
 
 window.addEventListener("contextmenu", e => e.preventDefault())
 
@@ -41,13 +50,18 @@ function onInit() {
     gGame.markedCount = 0
     gBoard.secsPassed = 0
     gLifeLost = 0
+    gHintUsed = 0
+    gHintIsOn = false
     gMines = []
 
     gBoard = buildBoard()
     renderBoard(gBoard)
     renderButtons()
     renderLives()
+    renderHints()
     renderSmiley()
+    resetTimer()
+    renderBestScore()
     console.log("gLevel.LIVES", gLevel.LIVES)
     console.log("gLevels[0].LIVES", gLevels[0].LIVES)
     console.log("gLevels[1].LIVES", gLevels[1].LIVES)
@@ -205,11 +219,11 @@ function renderButtons() {
 }
 
 function renderLives() {
-    var lives = genLives() 
+    var lives = genLives()
     var strHtmlLives = `Lives: ${lives}`
     var elLife = document.querySelector('.life')
     elLife.innerHTML = strHtmlLives
-    
+
 }
 
 function genLives() {
@@ -221,13 +235,46 @@ function genLives() {
     return strLives
 }
 
+function renderHints() {
+    var hints = genHints()
+    //`<button onclick="chooseLevel(this)">Hints: ${hints}</span>`
+    var strHtmlHints = `<span class="hints" onclick="onClickHint()">Hints: ${hints}</span>`
+    var elHint = document.querySelector('.hints')
+    elHint.innerHTML = strHtmlHints
+
+}
+
+function genHints() {
+    var hintsHtml = HINT
+    var usedhintsHtml = USED_HINT
+    var strHints = ''
+    for (var i = 0; i < gLevel.HINTS - gHintUsed; i++) {
+        strHints += hintsHtml
+    }
+
+    for (var i = 0; i < gHintUsed; i++) {
+        strHints += usedhintsHtml
+    }
+
+    return strHints
+}
+
+function onClickHint() {
+
+    gHintUsed++
+    gHintIsOn = true
+
+    renderHints()
+
+}
+
 function renderSmiley() {
     var strHtmlSmiley = `<button onclick="onInit()">`
     var normal = `${SMILEYNORMAL}`
     var win = `${SMILEYWIN}`
     var lose = `${SMILEYDEAD}`
     strHtmlSmiley += gGame.isOn ? normal : gIsvictory ? win : lose
-    
+
     console.log('gGame.isOn', gGame.isOn)
     console.log('gIsvictory', gIsvictory)
     strHtmlSmiley += `</button>`
@@ -242,41 +289,82 @@ function renderSmiley() {
 function onCellClicked(elCell, i, j) {
 
     // if (cell.isShown || !gGame.isOn) return
+   
 
     if (gGame.shownCount === 0) {
         placeMines(gBoard, i, j)
-
+        onTimer()
     }
 
-    if (!gBoard[i][j].isMarked) {
-        gBoard[i][j].isShown = true
-        if (!gBoard[i][j].isMine) {  
-            gGame.shownCount++
-            expandShown(i, j)
-        }
-        //LOSE: when clicking a mine, all the mines are revealed
-        else {
-            
-            if ((gLevel.LIVES - gLifeLost) === 1){
-                for (var i = 0; i < gMines.length; i++) {
-                    var currMine = gBoard[gMines[i].i][gMines[i].j]
-                    currMine.isShown = true
+    if (gHintIsOn) {
+        handleHint(i,j)
+
+  
+
+        
+
+    } else {
+
+        if (!gBoard[i][j].isMarked) {
+            gBoard[i][j].isShown = true
+            if (!gBoard[i][j].isMine) {
+                gGame.shownCount++
+                expandShown(i, j)
+            }
+            //LOSE: when clicking a mine, all the mines are revealed
+            else {
+
+                if ((gLevel.LIVES - gLifeLost) === 1) {
+                    for (var i = 0; i < gMines.length; i++) {
+                        var currMine = gBoard[gMines[i].i][gMines[i].j]
+                        currMine.isShown = true
+                    }
+                    gGame.isOn = false
+                    gIsvictory = false
+                    stopTimer()
+                    renderSmiley()
                 }
-                gGame.isOn = false
-                gIsvictory = false
-                renderSmiley()
-            } 
-            gLifeLost++
-            renderLives()
+                gLifeLost++
+                renderLives()
+            }
+
         }
 
         checkGameOver()
-        console.log('checkGameOver', checkGameOver())
 
     }
 
     renderBoard(gBoard)
 
+}
+
+function handleHint(i,j){
+
+    var revealedCells = []
+    
+    for (var idxI = i - 1; idxI <= i + 1; idxI++) {
+        if (idxI < 0 || idxI > gBoard.length - 1) continue
+        for (var idxJ = j - 1; idxJ <= j + 1; idxJ++) {
+            if (idxJ < 0 || idxJ > gBoard[idxI].length - 1) continue
+            if (!gBoard[idxI][idxJ].isShown) {
+                gBoard[idxI][idxJ].isShown = true
+                revealedCells.push({i:idxI,j:idxJ})
+            }
+        }
+    } 
+
+    renderBoard(gBoard)
+
+    setTimeout(() => {
+        for (var idxI = 0; idxI< revealedCells.length; idxI++) {
+       
+                    gBoard[revealedCells[idxI].i][revealedCells[idxI].j].isShown = false
+                
+                }
+
+        renderBoard(gBoard)
+        
+      }, 1000)
 }
 
 
@@ -297,7 +385,7 @@ function onCellMarked(elCell, i, j) {
         }
     }
     checkGameOver()
-    console.log('checkGameOver', checkGameOver())
+
     renderBoard(gBoard)
 }
 
@@ -322,17 +410,40 @@ function checkGameOver() {
         allMinesMarked = false
     }
 
-    console.log("allMinesMarked", allMinesMarked)
 
     if (gGame.shownCount === (gLevel.SIZE ** 2 - gLevel.MINES) && allMinesMarked) {
         gGame.isOn = false
         gIsvictory = true
-        console.log("gGame.isOn", gGame.isOn)
-        console.log("gIsvictory", gIsvictory)
+        checkBestScore()
+        renderBestScore()
         renderSmiley()
+        stopTimer()
         return true
     } else return false
 
+
+
+}
+
+function checkBestScore() {
+    gBestScoreLevel = 'bestScore' + gLevel.SIZE
+    console.log('gBestScoreLevel',gBestScoreLevel)
+    if (localStorage.getItem(gBestScoreLevel) === null) {
+        localStorage.setItem(gBestScoreLevel, elapsedTime)
+    } else if (localStorage.getItem(gBestScoreLevel) > elapsedTime) {
+        localStorage.setItem(gBestScoreLevel, elapsedTime)
+    }
+}
+
+function renderBestScore() {
+    var bestScore = localStorage.getItem(gBestScoreLevel)
+    if (bestScore !== null) {
+        var strHtmlBestScore = `Best Score: ${bestScore} seconds`
+    } else {
+        strHtmlBestScore = `Best Score: Not Set Yet`
+    }
+    var elBestScore = document.querySelector('.bestScore')
+    elBestScore.innerHTML = strHtmlBestScore;
 }
 
 //When user clicks a cell with no mines around, we need to open not only that cell, but also its neighbors.
@@ -350,7 +461,6 @@ function expandShown(i, j) {
                 if (!gBoard[idxI][idxJ].isShown && !gBoard[idxI][idxJ].isMarked) {
                     gBoard[idxI][idxJ].isShown = true
                     gGame.shownCount++
-                    console.log('gGame.shownCount', gGame.shownCount)
                     expandShown(idxI, idxJ)
                 }
             }
@@ -358,3 +468,39 @@ function expandShown(i, j) {
     }
 
 }
+
+function onTimer() {
+    if (!isRuning) {
+        startTime = Date.now()
+        timer = setInterval(update,50)
+        isRuning = true
+    }
+  }
+
+  function update() {
+    var currentTime = Date.now()
+    elapsedTime = currentTime - startTime
+    var seconds  = Math.floor(elapsedTime / 1000 % 60)
+    var milliSeconds  = Math.floor(elapsedTime % 1000 )
+    seconds = String(seconds).padStart(2,'0')
+    milliSeconds= String(milliSeconds).padStart(3,'0')
+
+    display.textContent = `${seconds}:${milliSeconds}`
+
+  }
+
+  function stopTimer() {
+    if (isRuning) {
+        clearInterval(timer)
+        elapsedTime = Date.now - startTime
+        isRuning = false
+    }
+  }
+
+  function resetTimer() {
+    clearInterval(timer)
+    isRuning = false
+    startTime = 0
+    elapsedTime = 0 
+    display.textContent = '00:000'
+  }
